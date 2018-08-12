d0dd2af6e6851fa59a25f867e920b587d90cc408
saurav <sauravpd58@gmail.com>
2018-08-13 02:15:49 +0530

    added changes

--- src/main/java/tests/loginTests.java
+++ src/main/java/tests/loginTests.java
@@ -10,7 +10,7 @@ import base.TestBase;
 import utilities.DataProviderUtil;
 import utilities.ExcelUtil;
 
-public class loginTest extends TestBase
+public class loginTests extends TestBase
 {
 	@Test(dataProviderClass = DataProviderUtil.class, dataProvider = "testData")
 	public void LoginTest(Hashtable<String,String> data)
